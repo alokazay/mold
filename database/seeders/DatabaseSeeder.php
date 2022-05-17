327be6b671ffdf061933012a3fee41c30f9b435b
Sergey S <alokazay@gmail.com>
2022-05-17 17:32:54 +0300

    add User Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $NOW = Carbon::now();
+        User::insert([
+            [
+                'id'=> 1,
+                'name' => 'Administrator',
+                'group_id ='=>1,
+                'email'=>'admin@test.net',
+                'password'=>Hash::make('1111'),
+                'remember_token'=>Hash::make(Hash::make('oK5sU4rM')),
+                'activation'=>1,
+                'created_at'=>$NOW,
+                'updated_at'=>$NOW,
+            ],
+        ]);
     }
 }
